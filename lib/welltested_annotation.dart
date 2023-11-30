c34100603ee9fdd50558b05c624d2f1a089d26e3
Yogesh Choudhary <youzendachoudhary22@gmail.com>
2023-11-30 11:14:48 +0530

    barrel file + configuration template

--- lib/welltested_annotation.dart
+++ lib/welltested_annotation.dart
@@ -6,20 +6,5 @@
  * and can't be edited, redistributed or used without a valid license of it's product (Welltested)[https://welltested.ai]
  */
 
-///Annotate classes with [Welltested] to generate unit tests for it's methods.
-///
-///Param: [excludedMethods] accepts functions to be excluded from testing.
-class Welltested {
-  final List<String> excludedMethods;
-
-  const Welltested({
-    this.excludedMethods = const [],
-  });
-}
-
-class Testcases {
-  final List<String> testcases;
-  const Testcases(
-    this.testcases,
-  );
-}
+export 'src/annotation.dart';
+export 'src/configuration.dart';
